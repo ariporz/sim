5357ac71c4632981dd63d993978fbf9cd5134700
Ari Porz <ariporz@users.noreply.github.com>
2018-09-29 12:42:46 +0200

    Initial creation.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var t = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + t);
+        }
+    }
+}
